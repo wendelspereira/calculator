07d6fca5d2a4ee7ac07fc3b1d08259b5ed1dd1d6
wendelspereira <wendelspereira@outlook.com>
2021-01-20 08:35:54 -0300

    conclusao da logica da calculadora

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
 import './Button.css'
 export default (props) => {
+
+    let classes = 'button '
+
+    classes += props.operation ? 'operation' : ''
+    classes += props.double ? 'double' : ''
+    classes += props.triple ? 'triple' : ''
+
     return (
-        <div className="button">{props.label}</div>
+        <div className={classes} onClick={e => props.click && props.click(props.label)}>
+            {props.label}
+        </div>
     )
 }
